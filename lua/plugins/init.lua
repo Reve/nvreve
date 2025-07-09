a083e1c27430aac8cd506aeb71ef428e42ff4260
Alexandru Gheorghita <alex.gheorghita@rotld.ro>
2025-07-09 16:02:49 +0300

    feat: changed colorscheme to gruvbox for more retro vibes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,12 +4,18 @@ return {
         dependencies = { { 'nvim-lua/plenary.nvim' } }
     },
 
+    -- {
+    --     'rose-pine/neovim',
+    --     name = 'rose-pine',
+    --     config = function()
+    --         vim.cmd('colorscheme rose-pine')
+    --     end
+    -- },
     {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        config = function()
-            vim.cmd('colorscheme rose-pine')
-        end
+        'ellisonleao/gruvbox.nvim',
+        priority = 1000,
+        config = true,
+        opts = ...
     },
 
     {
